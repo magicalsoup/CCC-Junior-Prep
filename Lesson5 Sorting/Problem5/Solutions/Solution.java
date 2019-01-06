023e539424a1d37df2f1e86744a9c4ae422c3e47
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-01-05 23:05:30 -0500

    Create Solution.java

---
+++ Lesson5 Sorting/Problem5/Solutions/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Solution{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n-- > 0){
+            System.out.println(sc.nextLong());
+        }
+        sc.close();
+    }
+}
