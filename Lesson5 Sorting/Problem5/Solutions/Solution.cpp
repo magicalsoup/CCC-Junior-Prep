79fd0f1eb344b4a0e5c0f2e5fae724aab3685753
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-01-05 23:22:19 -0500

    Create Solution.cpp

---
+++ Lesson5 Sorting/Problem5/Solutions/Solution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; scanf("%d", &n); // fast input
+    for(int i = 0; i < n; i++){
+        long p; scanf("%lld", &p); // fast input
+        printf("%lld\n", p); // fast output
+    }
+    return 0;
+}
