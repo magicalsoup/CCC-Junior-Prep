29bbefa07dda4d4bf6272a139a8b28a775765770
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-01-06 18:44:21 -0500

    Create Solution.java

---
+++ Lesson6 Simple-Intermediate Math/Problem4/Solutions/Solution.java
@@ -0,0 +1,9 @@
+import java.util.*;
+    public class Solution{
+      public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+    	  double v = sc.nextInt();
+    	  double c = 360 / v;
+    	  System.out.printf("%.6f", c); out.close();
+      }
+  }
