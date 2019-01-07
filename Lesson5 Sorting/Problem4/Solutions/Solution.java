4a9e389d387f0ef7f0137350e4e2b847502f53f5
Pete._.Mango <40186116+ASIANCLAN@users.noreply.github.com>
2019-01-06 19:03:27 -0500

    Update Solution.java

--- Lesson5 Sorting/Problem4/Solutions/Solution.java
+++ Lesson5 Sorting/Problem4/Solutions/Solution.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int array[] = new int[n];
+		int n = sc.nextInt(), array[] = new int[n]; //n is the length of the array
 		for(int i = 0; i < n; i++) {
 			array[i] = sc.nextInt();
 		}
@@ -11,21 +10,21 @@ public class Solution {
 		for(int j = 0; j < array.length; j++) {
 			System.out.print(array[j] + " ");
 		}
-		System.out.println();
+		System.out.println(); //print on a new line
 		
-		boolean isSorted = false;
-		while(!isSorted) {
+		boolean isSorted = false; //set a boolean to check if the array is sorted
+		while(!isSorted) { //while the array is not sorted,
 			isSorted = true;
-			for(int i = 0; i < array.length - 1; i++) {
-				if(array[i] > array[i + 1]) {
+			for(int i = 0; i < n - 1; i++) {
+				if(array[i] > array[i + 1]) { //bubble sort
 					int temp = array[i];
 					array[i] = array[i + 1];
 					array[i + 1] = temp;
 					isSorted = false;
-					for(int j = 0; j < array.length; j++) {
+					for(int j = 0; j < n; j++) {
 						System.out.print(array[j] + " ");
 					}
-					System.out.println();
+					System.out.println(); //print on a new line
 				}
 			}
 		}
