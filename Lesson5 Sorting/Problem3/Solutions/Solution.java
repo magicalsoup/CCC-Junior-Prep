5c79af3fb78adb24013f2724a5f0a5669001ddf4
Pete._.Mango <40186116+ASIANCLAN@users.noreply.github.com>
2019-01-06 19:07:16 -0500

    Update Solution.java

--- Lesson5 Sorting/Problem3/Solutions/Solution.java
+++ Lesson5 Sorting/Problem3/Solutions/Solution.java
@@ -2,28 +2,26 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for (int i = 0; i < n; i++) {
-			int swaps = 0;
-			int l = sc.nextInt();
-			int[] train = new int[l];
+		int n = sc.nextInt(); //number of test cases
+		for (int i = 0; i < n; i++) { 
+			int l = sc.nextInt, train = new int[l], swaps = 0; //l is the length of the array
 			for (int j = 0; j < l; j++) {
 				train[j] = sc.nextInt();
 			}
-			boolean isSorted = false;
-      while(!isSorted){
-        isSorted = true;
+			boolean isSorted = false; //boolean to check if the array is sorted
+      			while(!isSorted){ //while the array is not sorted
+       				isSorted = true;
 				for (int j = 0; j < l - 1; j++) {
 					if (train[j] > train[j + 1]) {
-				    int temp = train[j];
+				    		int temp = train[j]; //bubble sort
 						train[j] = train[j + 1];
 						train[j + 1] = temp;
-            isSorted = false;
-						swaps = swaps + 1;
+         					isSorted = false;
+						swaps = swaps + 1; //every time we make a swap, we add 1 to the counter
 					}
 				}
 			}
-			System.out.println("Optimal train swapping takes " + swaps + " swap(s).");
+			System.out.println("Optimal train swapping takes " + swaps + " swap(s)."); //print the counter
 		}
 	}
 }
