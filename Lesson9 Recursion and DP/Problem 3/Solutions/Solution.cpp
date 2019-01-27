924f4e7de5aa75dd397c5a75beb0e6c72e995ddd
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-01-27 15:33:20 -0500

    Create Solution.cpp

---
+++ Lesson9 Recursion and DP/Problem 3/Solutions/Solution.cpp
@@ -0,0 +1,8 @@
+string pairStar(String str) {
+  if(str.length() < 2)
+      return str;
+  if(str[0] == str[1])
+      return str[0] + "*" + pairStar(str.substr(1))
+      
+  return str[0] + pairStar(str.substr(1))
+}
