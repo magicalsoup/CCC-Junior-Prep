b22afa46369a52410b07e40822ad47b95979116b
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-01-27 15:33:37 -0500

    Update Solution.cpp

--- Lesson9 Recursion and DP/Problem 3/Solutions/Solution.cpp
+++ Lesson9 Recursion and DP/Problem 3/Solutions/Solution.cpp
@@ -2,7 +2,6 @@ string pairStar(String str) {
   if(str.length() < 2)
       return str;
   if(str[0] == str[1])
-      return str[0] + "*" + pairStar(str.substr(1))
-      
+      return str[0] + "*" + pairStar(str.substr(1))  
   return str[0] + pairStar(str.substr(1))
 }
